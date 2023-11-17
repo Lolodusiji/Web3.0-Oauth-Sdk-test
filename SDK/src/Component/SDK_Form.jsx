ab549a07a233387c3d8d25f25fa0a89b5c40f2eb
Lolodusiji <sholaodusiji@gmail.com>
2023-11-17 10:49:02 +0100

    form-complete

--- SDK/src/Component/SDK_Form.jsx
+++ SDK/src/Component/SDK_Form.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Chain from "../../public/chain.png"
+import "./SDK_Form.css"
 
 const SDK_Form = () => {
   return (
-    <div>SDK_Form</div>
-  )
+    <div>
+      <div className="container">
+        <div className="left_side">
+          <img src={Chain} alt="" />
+        </div>
+        <div className="right_side">
+          <textarea id="myTextarea" placeholder="Prompt here..."/>
+          <button>Submit</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SDK_Form
